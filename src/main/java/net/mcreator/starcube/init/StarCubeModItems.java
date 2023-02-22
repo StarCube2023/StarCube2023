6d8fc37d2b279ba77ced50f4b2d3ece5a71a649b
StarCube2023 <morgan@DESKTOP-LPH4V3A>
2023-02-22 17:16:25 +0100

    test

--- src/main/java/net/mcreator/starcube/init/StarCubeModItems.java
+++ src/main/java/net/mcreator/starcube/init/StarCubeModItems.java
@@ -13,11 +13,13 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.starcube.item.UraniumItem;
 import net.mcreator.starcube.StarCubeMod;
 
 public class StarCubeModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, StarCubeMod.MODID);
 	public static final RegistryObject<Item> URANIUM_BLOCK = block(StarCubeModBlocks.URANIUM_BLOCK, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> URANIUM = REGISTRY.register("uranium", () -> new UraniumItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
